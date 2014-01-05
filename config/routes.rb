8eea046d232e3e96cd330e0c344e10c7fc0efb64
Ivan Navarrete <crzivn@gmail.com>
2014-01-05 07:45:37 +0100

    Remove all routes until issue #351 is resolved.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+=begin
   resources :passwords,
     :controller => 'clearance/passwords',
     :only => [:create, :new]
@@ -18,4 +19,5 @@ Rails.application.routes.draw do
   get '/sign_in' => 'clearance/sessions#new', :as => 'sign_in'
   delete '/sign_out' => 'clearance/sessions#destroy', :as => 'sign_out'
   get '/sign_up' => 'clearance/users#new', :as => 'sign_up'
+=end
 end
